61558a89fb86073afa5727670aa8e5b5681ba7eb
Brian <karanjabrian382@gmail.com>
2025-01-07 15:49:46 +0300

    ver 2.0

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import FoodItem
-
-@admin.register(FoodItem)
-class FoodItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'quantity', 'expiry_date', 'is_expired')
-    list_filter = ('expiry_date',)
